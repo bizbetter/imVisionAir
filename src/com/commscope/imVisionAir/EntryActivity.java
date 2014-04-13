dadf3f354b53b9261edf840821456d4d73fac055
Yang SHEN <bizbetter@126.com>
2014-04-13 11:43:55 +0800

    F

--- src/com/commscope/imVisionAir/EntryActivity.java
+++ src/com/commscope/imVisionAir/EntryActivity.java
@@ -2,8 +2,11 @@ package com.commscope.imVisionAir;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class EntryActivity extends Activity {
+public class EntryActivity extends Activity implements View.OnClickListener {
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +14,17 @@ public class EntryActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button button = (Button)findViewById(R.id.buttonOK);
+        button.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.buttonOK){
+            TextView view1 = (TextView)findViewById(R.id.messageMain);
+            view1.setText("Good Test");
+        }
     }
 }
